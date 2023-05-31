8a66de355f5a6b96cf67e6290503156c1e6c3d40
Tanzil <tsarker@bgcengineering.ca>
2023-05-31 00:43:14 -0700

    prettify

--- src/asmCodeGenerator/runtime/RunTime.java
+++ src/asmCodeGenerator/runtime/RunTime.java
@@ -6,7 +6,7 @@ public class RunTime {
 	public static final String EAT_LOCATION_ZERO      = "$eat-location-zero";		// helps us distinguish null pointers from real ones.
 	public static final String INTEGER_PRINT_FORMAT   = "$print-format-integer";
 	public static final String BOOLEAN_PRINT_FORMAT   = "$print-format-boolean";
-	public static final String FLOAT_PRINT_FORMAT   = "$print-format-float";
+	public static final String FLOAT_PRINT_FORMAT     = "$print-format-float";
 	public static final String NEWLINE_PRINT_FORMAT   = "$print-format-newline";
 	public static final String SPACE_PRINT_FORMAT     = "$print-format-space";
 	public static final String BOOLEAN_TRUE_STRING    = "$boolean-true-string";
